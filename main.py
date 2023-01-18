79ba08c827a7b5c8168dc18cf4ed5c8c1d21f96c
aujuslia <77514367+aujuslia@users.noreply.github.com>
2023-01-17 23:31:23 -0800

    Update main.py

--- main.py
+++ main.py
@@ -10,17 +10,18 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='~', intents=intents)
 
 
-@bot.event
-async def on_ready():
-    print(f"Logged in as {bot.user}")
+# @bot.event
+# async def on_ready():
+#    print(f"Logged in as {bot.user}")
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-
+    
 @bot.command()
-async def hello(ctx):
-    await ctx.send("Choo choo! 🚅")
+async def fuckdavid(ctx):
+    channel = bot.get_channel(1052533505051070495)
+    await channel.send("fuck you @Davidvz13")
 
 
 bot.run(os.environ["DISCORD_TOKEN"])
